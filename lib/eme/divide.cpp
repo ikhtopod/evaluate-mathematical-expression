8a272fa34186006e353bc7846a78c9224cd52d14
reclue <vitaly@lifanoff.ru>
2020-11-27 20:27:22 +0300

    bugfix divide by zero

--- lib/eme/divide.cpp
+++ lib/eme/divide.cpp
@@ -1,10 +1,14 @@
 #include "divide.h"
 
+#include <limits>
+
 
 namespace reclue {
 
     double Divide::Calculate() {
-        return m_first->Calculate() / m_second->Calculate();
+        double first { m_first->Calculate() };
+        double second { m_second->Calculate() };
+        return second > std::numeric_limits<double>::min() ? first / second : 0.0;
     }
 
 }
